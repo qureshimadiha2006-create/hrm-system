2c075c74cb72e086aa08c5b9e4c3c2639a19e90b
Bilal <bilalsk.777999@gmail.com>
2026-04-14 14:58:45 +0530

    fix railway path

--- frontend/src/Department.js
+++ frontend/src/Department.js
@@ -15,12 +15,12 @@ function Department() {
   }, []);
 
   const fetchDepartments = async () => {
-    const res = await axios.get("http://localhost:5000/departments");
+    const res = await axios.get("https://hrm-system-madiha.onrender.com/departments");
     setDepartments(res.data);
   };
 
   const fetchDeletedDepartments = async () => {
-    const res = await axios.get("http://localhost:5000/deleted-departments");
+    const res = await axios.get("https://hrm-system-madiha.onrender.com/deleted-departments");
     setDeletedDepartments(res.data);
   };
 
@@ -31,12 +31,12 @@ function Department() {
     }
 
     if (editId) {
-      await axios.put(`http://localhost:5000/update-department/${editId}`, {
+      await axios.put(`https://hrm-system-madiha.onrender.com/update-department/${editId}`, {
         dept_name,
         description,
       });
     } else {
-      await axios.post("http://localhost:5000/add-department", {
+      await axios.post("https://hrm-system-madiha.onrender.com/add-department", {
         dept_name,
         description,
       });
@@ -52,14 +52,14 @@ function Department() {
 
   const deleteDepartment = async (id) => {
     if (window.confirm("Are you sure?")) {
-      await axios.delete(`http://localhost:5000/delete-department/${id}`);
+      await axios.delete(`https://hrm-system-madiha.onrender.com/delete-department/${id}`);
       fetchDepartments();
       fetchDeletedDepartments();
     }
   };
 
   const restoreDepartment = async (id) => {
-    await axios.put(`http://localhost:5000/restore-department/${id}`);
+    await axios.put(`https://hrm-system-madiha.onrender.com/restore-department/${id}`);
     fetchDepartments();
     fetchDeletedDepartments();
   };
@@ -91,7 +91,6 @@ function Department() {
         {editId ? "Update" : "Add"}
       </button>
 
-      {/* ✅ ACTIVE TABLE */}
       <h3>Active Departments</h3>
 
       <table border="1" style={{ margin: "auto" }}>
@@ -121,7 +120,6 @@ function Department() {
         </tbody>
       </table>
 
-      {/* ✅ DELETED TABLE */}
       <h3>Deleted Departments</h3>
 
       <table border="1" style={{ margin: "auto" }}>
